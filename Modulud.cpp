aa874f71e8e392dd5744ad904390f6388c2042f2
Siddharth292004 <sidharth9509@gmail.com>
2024-08-12 23:44:41 +0530

    new update

---
+++ Modulud.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 2 ;
+    int y =  8;
+        cout << (-x) % y <<endl;
+        cout << x % (-y) <<endl;
+        cout << x % y << endl;
+        cout << x % x << endl;
+
+
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout << "ENTER THE NUMBER  A : ";
+    cin >>a;
+    cout << "ENTER THE NUMBER  B : ";
+    cin >>b;
+    cout << "ENTER THE NUMBER  C : ";
+    cin >>c;
+    cout <<"SUM OF THREE NUMBER : "<< a + b + c;
+}
